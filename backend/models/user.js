13d46cc3741fcb9b3a3e3bfc13861e0c43597859
Your Name <najat_sf@hotmail.com>
2024-05-24 13:47:48 +0200

    add book routes and models

---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      max: 50,
+    },
+   
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
